980896de86239c871076915fe096143101324641
BubbleThy12 <unicornvo123@gmail.com>
2025-12-20 18:49:30 +1000

    finish filter files and search feature

--- app/(root)/[type]/page.tsx
+++ app/(root)/[type]/page.tsx
@@ -2,12 +2,17 @@ import { Props } from "@/components/ActionsModalContent";
 import Card from "@/components/Card";
 import Sort from "@/components/Sort";
 import { getFiles } from "@/lib/actions/file.actions";
+import { getFileTypesParams } from "@/lib/utils";
 import { Models } from "node-appwrite";
 
-const Page = async ({ params }: SearchParamProps) => {
+const Page = async ({ searchParams, params }: SearchParamProps) => {
   const type = ((await params)?.type as string) || "";
+  const searchText = ((await searchParams)?.query as string) || "";
+  const sort = ((await searchParams)?.sort as string) || "";
 
-  const files = await getFiles();
+  const types = getFileTypesParams(type) as FileType[];
+
+  const files = await getFiles({ types, searchText, sort });
 
   return (
     <div className="page-container">
@@ -34,9 +39,11 @@ const Page = async ({ params }: SearchParamProps) => {
         <section className="file-list">
           {files.documents.map(
             (
-              file: Models.Document & Props & {
-                owner: Models.Document & { fullName: string };
-              }
+              file: Models.Document &
+                Props & {
+                  owner: Models.Document & { fullName: string };
+                  users: string[];
+                }
             ) => (
               <Card key={file.$id} file={file} />
             )
