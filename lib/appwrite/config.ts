3e89a47136dacaa2803f4ee1d5b6078392dd42e6
BubbleThy12 <unicornvo123@gmail.com>
2025-12-22 20:49:02 +1000

    merge to feature/IECTD-2_FileUploader

--- lib/appwrite/config.ts
+++ lib/appwrite/config.ts
@@ -1,5 +1,5 @@
 export const appwriteConfig = {
-  endpointuUrl: process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!,
+  endpointUrl: process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!,
   projectId: process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID!,
   databaseId: process.env.NEXT_PUBLIC_APPWRITE_DATABASE!,
   usersCollectionId: process.env.NEXT_PUBLIC_APPWRITE_USERS_COLLECTION!,
